08ce6fd256e85c0b51e7d4577418ebf6831c0e67
jzxn11 <149028308+jzxn11@users.noreply.github.com>
2024-06-29 22:33:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static('public'));
 
 const db = mysql.createConnection({
-  host: '127.0.0.1',
+  host: '127.0.0.1:3306',
   user: 'root',
   password: 'telCatMiau77698*',
   database: 'captive_portal'
