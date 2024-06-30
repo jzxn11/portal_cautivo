fe526bd542cf066643382f1814c6a5ef2ebc1d4a
jzxn11 <149028308+jzxn11@users.noreply.github.com>
2024-06-30 00:36:58 -0400

    Update meraki.js

--- public/js/meraki.js
+++ public/js/meraki.js
@@ -49,6 +49,24 @@ function login(){
 
     // Complete Login
     authUser();
+
+    //Send data
+
+    fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ data.name, data.email })
+    })
+    .then(response => response.text())
+    .then(data => {
+        alert(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
 }
 
 // Helper function to parse URL
