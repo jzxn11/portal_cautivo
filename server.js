150b96249198b0f593a9ebeb72b06dd385f7dd98
jzxn11 <149028308+jzxn11@users.noreply.github.com>
2024-06-30 10:05:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,32 +3,35 @@
 var express = require("express");
 var app = express();
 
-// Express Midleware  - BodyParser
+// Express Middleware - BodyParser
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// database
+// Database
 const mysql = require('mysql');
 const path = require('path');
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: 'telCatMiau77698*',
   database: 'captive_portal',
   port: 3306
 });
 db.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.error('Error conectando a la base de datos:', err.stack);
+    return;
+  }
   console.log('Conectado a la base de datos MySQL.');
 });
 
-// serve static public site files
+// Serve static public site files
 app.use(express.static('public'));
 
 /* serves main page */
 app.get("/", function(req, res) {
-   res.sendfile('public/index.html')
+   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Ruta para recibir datos de meraki.js
@@ -36,17 +39,15 @@ app.post('/submit', (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
 
-  const query = "INSERT INTO users (name, email) VALUES ('"+name+"','"+email+"');";
+  const query = 'INSERT INTO users (name, email) VALUES (?, ?)';
   db.query(query, [name, email], (err, result) => {
     if (err) throw err;
     res.send('Datos recibidos e insertados en la base de datos.');
   });
 });
 
-// Servir archivos estáticos desde el directorio "public"
-app.use(express.static(path.join(__dirname, 'public')));
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
